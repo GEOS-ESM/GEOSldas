15b6fa2b4b3c31e84b77e0add97f011b6d039fa1
Rolf Reichle <rolf.reichle@nasa.gov>
2020-08-18 11:06:33 -0400

    edited comment

--- src/Components/GEOSldas_GridComp/GEOSmetforce_GridComp/LDAS_Forcing.F90
+++ src/Components/GEOSldas_GridComp/GEOSmetforce_GridComp/LDAS_Forcing.F90
@@ -4672,7 +4672,7 @@ contains
        ! compute indices for nearest neighbor interpolation from GEOSgcm grid
        ! to tile space
           if( isCubed ) then ! cs grid
-             ! i_indg and j_indg are changed to LatLon grid
+             ! cube-sphere grid of forcing data must match cube-sphere grid associated with tile space
              do k=1,N_cat
                 i1(k) = tile_coord(k)%i_indg
                 j1(k) = tile_coord(k)%j_indg
