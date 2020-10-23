2fb8cf85b412dc4ccb53d2dc0ecb4dfb6abdf212
gmao-jkolassa <jana.kolassa@nasa.gov>
2020-10-23 12:34:15 -0400

    added 'rc=status' to SurfParams_init call

--- src/Components/GEOSldas_GridComp/GEOS_LdasGridComp.F90
+++ src/Components/GEOSldas_GridComp/GEOS_LdasGridComp.F90
@@ -425,8 +425,8 @@ contains
     VERIFY_(STATUS)
     call MAPL_GetResource ( MAPL, LSM_CHOICE, Label="LSM_CHOICE:", DEFAULT=1, RC=STATUS)
     VERIFY_(STATUS)
-    call SurfParams_init(LAND_PARAMS,LSM_CHOICE)
-
+    call SurfParams_init(LAND_PARAMS,LSM_CHOICE,rc=status)
+    VERIFY_(STATUS)
     
     call MAPL_GetResource(MAPL, grid_type,Label="GEOSldas.GRID_TYPE:",RC=STATUS)
     VERIFY_(STATUS)
