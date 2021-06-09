eeba0cd200bc406d77a3e858fa7bfef3d85ac39d
Weiyuan Jiang <weiyuan.jiang@nasa.gov>
2021-06-09 09:45:46 -0400

    minor fix

--- src/Components/GEOSldas_GridComp/GEOSlandassim_GridComp/clsm_ensupd_upd_routines.F90
+++ src/Components/GEOSldas_GridComp/GEOSlandassim_GridComp/clsm_ensupd_upd_routines.F90
@@ -1444,7 +1444,7 @@ contains
 
     ! move up the allocation. The input should be allocated in debug mode although it is not used
     ! allocate and assemble tile_data_l
-    allocate(tile_data_l(N_catl,N_fields,N_ens))
+    allocate(tile_data_l(0,0,0))
     
     call get_tiles_in_halo( N_catl, N_fields, N_ens, tile_data_l, tile_coord_l,  &
          N_catf, tile_coord_f, N_catl_vec, low_ind, xhalo, yhalo,                &
@@ -1465,7 +1465,8 @@ contains
     call get_obs_pred_comm_helper( N_catl, N_ens, N_TbuniqFreqAngRTMid,          &
          get_sfmc_lH, get_rzmc_lH, get_tsurf_lH, get_FT_lH, get_Tb_lH, N_fields)
     
-    
+    if(allocated(tile_data_l)) deallocate(tile_data_l) 
+    allocate(tile_data_l(N_catl,N_fields,N_ens))
     call get_obs_pred_comm_helper( N_catl, N_ens, N_TbuniqFreqAngRTMid,          &
          get_sfmc_lH, get_rzmc_lH, get_tsurf_lH, get_FT_lH, get_Tb_lH, N_fields, &
          option=1, tile_data=tile_data_l,                                        &
@@ -1478,7 +1479,6 @@ contains
          N_catf, tile_coord_f, N_catl_vec, low_ind, xhalo, yhalo,                &
          N_catlH, tile_data_lH=tile_data_lH )    
     
-    if (allocated(tile_data_l))  deallocate(tile_data_l)
     
     ! read out sfmc, rzmc, etc. from tile_data_lH    
     
@@ -1504,6 +1504,8 @@ contains
     
 #endif
     
+    if (allocated(tile_data_l))  deallocate(tile_data_l)
+
     ! ----------------------------------------------------------------
     !
     ! Get additional grid/tile information that is needed to map from tile
