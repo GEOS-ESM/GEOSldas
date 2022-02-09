93ef260d928abbd582df6c19c7270605de76c62f
Rolf Reichle <rolf.reichle@nasa.gov>
2022-02-09 11:42:39 -0500

    changed default deflate_level to 3 based on L4_SM;  added comments

--- src/Applications/LDAS_App/util/compress_nc.sh
+++ src/Applications/LDAS_App/util/compress_nc.sh
@@ -1,8 +1,10 @@
 #!/usr/local/bin/bash
+#
+# compress bit-shaved nc output
 
 usage(){
 	echo -e "\nUsage: $0 <directory>
-	<directory> - name of the directory
+	<directory> = name of output directory with bit-shaved nc files
 "
 	exit 1;
 }
@@ -12,8 +14,8 @@ if [[ $# -lt 1 ]]; then
 	exits;
 fi
 
-# deflation_level 1-9, higher level (e.g. 9) takes longer 
-deflate_level=5
+# deflation_level 1-9; higher level (e.g. 9) takes longer; recommended: 3 (based on L4_SM) 
+deflate_level=3
 
 for file in $(find $1 -name "*.nc4" -type f); do
 	#echo "Processing $file..."
