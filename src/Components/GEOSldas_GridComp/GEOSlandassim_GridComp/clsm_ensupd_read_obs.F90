08b3f853b266c35f65b3e68f0e48469d6054ac6e
amfox37 <amfox37@gmail.com>
2023-05-31 15:47:57 -0600

    added timing statements

--- src/Components/GEOSldas_GridComp/GEOSlandassim_GridComp/clsm_ensupd_read_obs.F90
+++ src/Components/GEOSldas_GridComp/GEOSlandassim_GridComp/clsm_ensupd_read_obs.F90
@@ -1551,7 +1551,7 @@ contains
   
   ! ****************************************************************************
 
-  subroutine read_obs_sm_ASCAT_EUMET(                                  &
+  subroutine read_obs_sm_ASCAT_EUMET(                            &
        work_path, exp_id,                                        &
        date_time, dtstep_assim, N_catd, tile_coord,              &
        tile_grid_d, N_tile_in_cell_ij, tile_num_in_cell_ij,      &
@@ -1569,6 +1569,7 @@ contains
     !
     !  Q. Liu, Nov. 2019.  
     ! based on read_obs_sm_ASCAT
+    ! Minor updates A. Fox, march 2023   
     ! --------------------------------------------------------------------
         
     implicit none
@@ -1628,6 +1629,7 @@ contains
     type(date_time_type) :: date_time_upp
     
     integer :: i, ind, N_tmp, N_files
+    integer :: start_time, end_time, elapsed_time
 
     character(300), dimension(:), allocatable :: fnames
  
@@ -1658,6 +1660,8 @@ contains
     ! ---------------
     
     ! initialize
+
+    start_time = system_clock()
     
     found_obs = .false.
 
@@ -1737,7 +1741,15 @@ contains
     end if
     
     close(10,status='delete')
+
+    ! Get the end time
+    end_time = system_clock()
+
+    ! Calculate the elapsed time in milliseconds
+    elapsed_time = (end_time - start_time) * 1000 / system_clock_rate()
     
+    write (logunit,*) 'Elapsed time file names: ', elapsed_time, ' milliseconds'
+
     ! read observations:
     !
     ! 1.) read N_tmp observations and their lat/lon info from file
@@ -1749,6 +1761,9 @@ contains
     ! ----------------------------------------------------------------
     !
     ! 1.) read N_tmp observations and their lat/lon info from file
+
+
+    start_time = system_clock()
 
     ! read and process data if files are found
     allocate(tmp1_lon(max_rec))
@@ -1872,6 +1887,14 @@ contains
     deallocate(tmp1_lat)
     deallocate(tmp1_obs) 
 
+    ! Get the end time
+    end_time = system_clock()
+
+    ! Calculate the elapsed time in milliseconds
+    elapsed_time = (end_time - start_time) * 1000 / system_clock_rate()
+    
+    write (logunit,*) 'Elapsed time bufr read: ', elapsed_time, ' milliseconds'
+
     ! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     !
     ! SOME QC SHOULD BE DONE HERE!!!
@@ -1885,6 +1908,8 @@ contains
     ! 2.) for each observation
     !     a) determine grid cell that contains lat/lon
     !     b) determine tile within grid cell that contains lat/lon
+
+    start_time = system_clock()
 
     if (N_tmp>0) then
        
@@ -1970,6 +1995,15 @@ contains
        end if
        
     end if
+
+
+        ! Get the end time
+    end_time = system_clock()
+
+    ! Calculate the elapsed time in milliseconds
+    elapsed_time = (end_time - start_time) * 1000 / system_clock_rate()
+    
+    write (logunit,*) 'Elapsed time tile allocation: ', elapsed_time, ' milliseconds'
     
     ! clean up
     
@@ -8274,6 +8308,11 @@ do i=1,N_catd
       
       if ( abs(tile_coord(i)%com_lat-sclprm_lat(j_ind))>tol  .or.             &
            abs(tile_coord(i)%com_lon-sclprm_lon(i_ind))>tol        ) then
+         if (logit) write (logunit,*) 'ierr2, i = ', ierr2, i  
+         if (logit) write (logunit,*) 'start, icount', start, icount
+         if (logit) write (logunit,*) 'tile_coord(i)%com_lat, this_lat, sclprm_lat(j_ind)', tile_coord(i)%com_lat, this_lat, sclprm_lat(j_ind)
+         if (logit) write (logunit,*) 'tile_coord(i)%com_lon, this_lon, sclprm_lon(i_ind)', tile_coord(i)%com_lon, this_lon, sclprm_lon(i_ind)
+         if (logit) write (logunit,*) 'pp, sclprm_lon, sclprm_lat, sclprm_mean_obs(328:334,579) ', pp, sclprm_lon(1), sclprm_lat(1), sclprm_mean_obs(328:334, 579)
          err_msg = 'something wrong'
          call ldas_abort(LDAS_GENERIC_ERROR, Iam, err_msg)
       end if
@@ -8288,6 +8327,10 @@ do i=1,N_catd
          
          ! scale via standard normal deviates
          
+         if (mod(i, 100) == 0) then
+            write(logunit,*) 'Found! ', i, pp, j_ind, i_ind, tmp_obs(i), sclprm_mean_obs(j_ind, i_ind), sclprm_std_mod(j_ind, i_ind), sclprm_std_obs(j_ind, i_ind)
+         endif
+
          tmpreal = sclprm_std_mod(j_ind, i_ind)/sclprm_std_obs(j_ind, i_ind) 
          
          tmp_obs(i) = sclprm_mean_mod(j_ind, i_ind)                       &
@@ -8296,7 +8339,7 @@ do i=1,N_catd
          ! scale observation error std
          
          tmp_std_obs(i) = tmpreal*tmp_std_obs(i)
-
+         
       else
          
          tmp_obs(i) = this_obs_param%nodata
@@ -8310,7 +8353,7 @@ end do
 deallocate(sclprm_lon)     
 deallocate(sclprm_lat)          
 deallocate(sclprm_mean_obs)     
-deallocate(sclprm_std_obs)  
+deallocate(sclprm_std_obs)      
 deallocate(sclprm_mean_mod)     
 deallocate(sclprm_std_mod)      
 
